e68b3d73c011c65752baa7bbcd00c7c00b6e1189
mustafaDabah <mustafadabah5555@gmail.com>
2022-09-20 21:53:22 +0200

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,11 @@
-console.log(Math.floor(1));
-console.log(Math.ceil(1));
+/**
+ * This file is just a silly example to show everything working in the browser.
+ * When you're ready to start on your site, clear the file. Happy hacking!
+ **/
 
-let theName: string = 'msuss';
-let age: number = 40 ;
-let hire:boolean  = true;
-let all: any  = true;
-let allVars: string | number ; // any 
+import confetti from 'canvas-confetti';
 
-// hire = 2
-all = 1 
-
-allVars = 3 ;
-allVars = '3' ;
-
-// Type Annotations With Arrays
-
-let allTwo: string | number | boolean ;
-allTwo = 'a';
-allTwo = 100 ;
-allTwo = true
-
-let myFriend:string[] = ['m' , 'n' , 'c' ];
-for(let i = 0 ; i < myFriend.length ; i++) {
-    console.log(myFriend[i].repeat(3))
-}
-
-// Type Annotations With Multidimensional Arrays
-
-let arrayTwo:(number | string)[] = [1 , 2 ,3 , 'f' , 'v'];
-let nestArray:(number | string | boolean[])[] = [1 , 2 ,3 , 'f' , 'v' , [false , true]];
-let arrayFour:(number | string | boolean[] |boolean)[] = [1 , 2 ,3 , 'f' , 'v' , [false , true] , true , false]
-
-// Type Annotations With Function
-let showMes = true;
-function showDetails(name: string, age: number , salary: number):string {
-    // let n = 20 ;
-    if(showMes) {
-        return `hello ${name} age is ${age} salary ${salary}`
-    }
-
-    return 'there is no data'
-}
-
-console.log(showDetails('Osama' , 40 , 5000 ));
-
-// Function Optional and Default Parameters
-function showData(name: string , age: number , country?: string) {
-    return `${name} - ${age} - ${country}`
-}
-
-console.log(showData('mmm' , 40 ))
-
-// Function Rest Parameter
-function addAll(...nums:number[]):number {
-    let result = 0 ;
-    for(let i = 0 ; i < nums.length ; i++) {
-        result += nums[i];
-    }
-    return result;
-}
-
-console.log(addAll(10 , 20 , 30 , 40 , +true));
-
-// Type Annotations With Anonymous And Arrow Function
-const addN = (num:number , num1:number):number => num + num1
-console.log(addN(1 , 2))
-
-
+confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
+  resize: true,
+  useWorker: true,
+})({ particleCount: 200, spread: 200 });
