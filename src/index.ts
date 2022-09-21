eabf24e7c5b24f6173a3f0ac9aafaa49fecdb992
mustafaDabah <mustafadabah5555@gmail.com>
2022-09-21 23:34:49 +0200

    finish todolist with js

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,65 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import {v4 as uuidV4} from 'uuid'
 
-import confetti from 'canvas-confetti';
+const list = document.querySelector<HTMLUListElement>('#list');
+const button = document.getElementById('button') as HTMLButtonElement ;
+const form = document.getElementById('#new-task-form') as HTMLFormElement ;
+const input = document.querySelector<HTMLInputElement>('#new-task-title');
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+interface Task {
+    id:string,
+    title:string,
+    completed:boolean,
+    createdAt:Date
+}
+
+let tasks:[] = JSON.parse(localStorage.getItem("TODOS"))
+tasks.forEach(addListItem);
+console.log(tasks)
+
+button?.addEventListener('click', e => {
+    e.preventDefault();
+
+    if(input?.value == '' || input?.value == null) return;
+
+    const newTask:Task = {
+        id:uuidV4(),
+        title:input.value,
+        completed:true,
+        createdAt:new Date()
+    }
+
+    tasks.push(newTask);
+    setLocalStorageItems();
+
+
+    addListItem(newTask);
+    input.value = ''
+});
+function addListItem(newTask:Task):void {
+    const item = document.createElement('li')
+    const label = document.createElement('label')
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+
+    label.append(checkbox , newTask.title);
+    checkbox.checked = newTask.completed
+    item.append(label);
+    list?.append(item);
+
+    checkbox.addEventListener('change', e => {
+        newTask.completed = checkbox.checked
+        
+        setLocalStorageItems();
+    })
+
+}
+
+function setLocalStorageItems() {
+    localStorage.setItem('TODOS' , JSON.stringify(tasks))
+}
+
+function loadTasks(): Task[] {
+    const taskJSON = localStorage.getItem("TODOS")
+    if (taskJSON == null) return []
+    return JSON.parse(taskJSON)
+  }
