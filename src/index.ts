c7924ef9b35e3226c8cd9a1bdebb93796197df37
mustafaDabah <mustafadabah5555@gmail.com>
2022-09-22 21:57:15 +0200

    add AnalogClock

--- src/index.ts
+++ src/index.ts
@@ -1,65 +1,18 @@
-import {v4 as uuidV4} from 'uuid'
+const secondsHand = document.getElementById('seconds-hand') as HTMLDivElement;
+const hoursHand = document.getElementById('hours-hand') as HTMLDivElement;
+const minutesHand = document.getElementById('minutes-hand') as HTMLDivElement;
 
-const list = document.querySelector<HTMLUListElement>('#list');
-const button = document.getElementById('button') as HTMLButtonElement ;
-const form = document.getElementById('#new-task-form') as HTMLFormElement ;
-const input = document.querySelector<HTMLInputElement>('#new-task-title');
+function getTime():void {
+    const now: Date = new Date();
+    const seconds: number = now.getSeconds();
+    const hours: number = now.getHours();
+    const minutes: number = now.getMinutes();
+    const timeInterval: number = 6 ; 
+    console.log(seconds);
 
-interface Task {
-    id:string,
-    title:string,
-    completed:boolean,
-    createdAt:Date
+    secondsHand.style.transform = `rotate(${seconds * timeInterval}deg)`
+    minutesHand.style.transform = `rotate(${minutes * timeInterval + seconds / 10 }deg)`
+    hoursHand.style.transform = `rotate(${hours * 30 / 2}deg)`
 }
 
-let tasks:[] = JSON.parse(localStorage.getItem("TODOS"))
-tasks.forEach(addListItem);
-console.log(tasks)
-
-button?.addEventListener('click', e => {
-    e.preventDefault();
-
-    if(input?.value == '' || input?.value == null) return;
-
-    const newTask:Task = {
-        id:uuidV4(),
-        title:input.value,
-        completed:true,
-        createdAt:new Date()
-    }
-
-    tasks.push(newTask);
-    setLocalStorageItems();
-
-
-    addListItem(newTask);
-    input.value = ''
-});
-function addListItem(newTask:Task):void {
-    const item = document.createElement('li')
-    const label = document.createElement('label')
-    const checkbox = document.createElement('input')
-    checkbox.type = 'checkbox'
-
-    label.append(checkbox , newTask.title);
-    checkbox.checked = newTask.completed
-    item.append(label);
-    list?.append(item);
-
-    checkbox.addEventListener('change', e => {
-        newTask.completed = checkbox.checked
-        
-        setLocalStorageItems();
-    })
-
-}
-
-function setLocalStorageItems() {
-    localStorage.setItem('TODOS' , JSON.stringify(tasks))
-}
-
-function loadTasks(): Task[] {
-    const taskJSON = localStorage.getItem("TODOS")
-    if (taskJSON == null) return []
-    return JSON.parse(taskJSON)
-  }
+setInterval(() => getTime(), 100)
